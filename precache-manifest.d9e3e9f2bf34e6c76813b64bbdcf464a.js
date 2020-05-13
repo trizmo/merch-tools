19775955c96f95bc1c60b7009807fa0d13fcc69a
Tristan Perera <tristan.e.perera@gmail.com>
2020-05-12 21:53:56 -0700

    Updates

---
+++ precache-manifest.d9e3e9f2bf34e6c76813b64bbdcf464a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe79fc94af29ad52a3b89371d175675",
+    "url": "/merch-tools/index.html"
+  },
+  {
+    "revision": "aa1b4897777b253f3556",
+    "url": "/merch-tools/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "074b677db0fa3eb89aff",
+    "url": "/merch-tools/static/js/2.2ebf0915.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/merch-tools/static/js/2.2ebf0915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1b4897777b253f3556",
+    "url": "/merch-tools/static/js/main.c19fa47c.chunk.js"
+  },
+  {
+    "revision": "1dfcf3c343c6977f4d83",
+    "url": "/merch-tools/static/js/runtime-main.179806a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/merch-tools/static/media/logo.5d5d9eef.svg"
+  }
+]);
