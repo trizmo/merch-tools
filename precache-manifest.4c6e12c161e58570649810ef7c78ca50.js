d43600cfe91e28c9b33e23f2e6c603d6f962fc72
Tristan Perera <tristan.e.perera@gmail.com>
2020-05-20 15:55:06 -0700

    Updates

---
+++ precache-manifest.4c6e12c161e58570649810ef7c78ca50.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c585b7a503d6246f91abb225a1bd66c",
+    "url": "/merch-tools/index.html"
+  },
+  {
+    "revision": "3bd72046173932f92ac1",
+    "url": "/merch-tools/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cfb8c9cc69133d09e52d",
+    "url": "/merch-tools/static/css/main.c3f0fd14.chunk.css"
+  },
+  {
+    "revision": "3bd72046173932f92ac1",
+    "url": "/merch-tools/static/js/2.05925efa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/merch-tools/static/js/2.05925efa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb8c9cc69133d09e52d",
+    "url": "/merch-tools/static/js/main.e52d30c4.chunk.js"
+  },
+  {
+    "revision": "1dfcf3c343c6977f4d83",
+    "url": "/merch-tools/static/js/runtime-main.179806a1.js"
+  },
+  {
+    "revision": "c110854f4d5efa4be2dc690c9f8292ed",
+    "url": "/merch-tools/static/media/car1.c110854f.jpg"
+  },
+  {
+    "revision": "3ee503954211b8392a5aed872244cca3",
+    "url": "/merch-tools/static/media/car2.3ee50395.jpg"
+  },
+  {
+    "revision": "e5670bce91a2f7311808728cc95daf03",
+    "url": "/merch-tools/static/media/car3.e5670bce.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/merch-tools/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/merch-tools/precache-manifest.9877c7588f0198ca42059e33f66a9402.js"
+  "/merch-tools/precache-manifest.4c6e12c161e58570649810ef7c78ca50.js"
 );
 
 self.addEventListener('message', (event) => {
